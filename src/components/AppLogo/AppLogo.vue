190c88640d09263d82d8ec74d55bc2b34c87218c
Jennafer Crump <jenna.crump2@gmail.com>
2019-01-30 18:16:15 -0600

    Added Logo Component
    Creation of a logo component to be used throughout the app.

---
+++ src/components/AppLogo/AppLogo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="logo">
+    <span class="roof">^</span>
+    <span class="title">Little Home Cafe</span>
+  </div>
+</template>
+
+<script lang="ts" src="./AppLogo.ts"></script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css?family=Poiret+One|Quicksand:400,500,700");
+
+.logo {
+  font-family: "Poiret One", cursive;
+  font-size: 2em;
+
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 45px;
+}
+
+.roof {
+  font-size: 2.9em;
+  margin-left: 193px;
+  margin-bottom: 0;
+  padding-bottom: 0;
+}
+
+.title {
+  margin-top: -92px;
+  margin-left: 131px;
+}
+</style>
+
